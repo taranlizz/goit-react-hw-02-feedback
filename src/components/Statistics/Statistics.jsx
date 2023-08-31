d355a0ac365921119012a8a98782dbb833d2750c
Elizaveta Taran <lizataran103@gmail.com>
2023-08-31 23:06:03 +0300

    add markup

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,20 @@
+import css from './Statistics.module.css';
+
+export const Statistics = ({
+  good,
+  bad,
+  neutral,
+  total,
+  positivePercentage,
+}) => {
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Bad: {bad}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Total: {total}</p>
+      <p>Positive feedback: {positivePercentage}%</p>
+    </div>
+  );
+};
